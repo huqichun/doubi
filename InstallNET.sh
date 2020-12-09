3897e2f24eb383e0276ac04ab2b32112472d5a11
CCNV <huqichun@users.noreply.github.com>
2020-12-09 08:48:53 -0600

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -700,7 +700,7 @@ d-i debian-installer/allow_unauthenticated boolean true
 
 tasksel tasksel/first multiselect minimal
 d-i pkgsel/update-policy select none
-d-i pkgsel/include string openssh-server
+d-i pkgsel/include string openssh-server curl
 d-i pkgsel/upgrade select none
 
 popularity-contest popularity-contest/participate boolean false
